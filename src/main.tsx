ade252abaf3226c07b29064e3fb463a30aac2825
hugiparquet-ui <hugiparquet@gmail.com>
2026-07-25 04:19:03 -0300

    eliminado servicio walker

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,7 @@ import App from './App.tsx'
 import './index.css'
 
 // ✅ NO registrar Service Worker (para evitar el cache de errores)
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('/sw.js')
-//       .then(reg => console.log('✅ SW registrado:', reg))
-//       .catch(err => console.log('❌ SW error:', err))
-//   })
-// }
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
